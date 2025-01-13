7234faf70486355952065539d6c1b7c83658bb25
ormor <htth815@gmail.com>
2025-01-13 14:01:12 +0900

    Move: API 문서내용에 맞게 /members 하위 경로에 위치한 페이지 경로 일괄 수정 - 가입안내 페이지 : members/page.tsx - 로그인 페이지 : members/login/page.tsx - 개인정보수정 페이지 : members/[memberid]/page.tsx (#17)

---
+++ src/app/members/login/page.tsx
@@ -0,0 +1,8 @@
+// 로그인 페이지
+export default function Page() {
+  return (
+    <main>
+      <h2>Login Page</h2>
+    </main>
+  );
+}
---
+++ src/app/members/page.tsx
@@ -0,0 +1,8 @@
+// 가입안내 페이지
+export default function Page() {
+  return (
+    <main>
+      <h2>Join Page</h2>
+    </main>
+  );
+}

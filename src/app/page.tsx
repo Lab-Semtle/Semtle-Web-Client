313a930070179db13883d0b92032a4e49f66fd2b
한태현 <xogus5343@gmail.com>
2025-01-19 13:23:00 +0900

    Fix: 네비게이션 바 버그 수정(#1-fix)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image';
-//import NavigationBar from '@/components/Navigation';
-import Footer from '@/components/Footer';
+//import Image from 'next/image';
+import NavigationBar from '@/components/Navigation';
+//import Footer from '@/components/Footer';
 export default function Home() {
   return (
     <main>
-      {/* <NavigationBar /> */}
-      <Footer />
+      <NavigationBar />
+      {/*<Footer />*/}
     </main>
   );
 }

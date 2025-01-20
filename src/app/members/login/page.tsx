e781bbb48a4640feeb9e069d1299496c873d9e9b
or-m-or <htth815@gmail.com>
2025-01-20 15:20:52 +0900

    Move: 회원정보수정페이지 디렉토리명 변경 및 이동

--- src/app/members/login/page.tsx
+++
@@ -1,8 +0,0 @@
-// 로그인 페이지
-export default function Page() {
-  return (
-    <main>
-      <h2>Login Page</h2>
-    </main>
-  );
-}
--- src/app/members/page.tsx
+++
@@ -1,8 +0,0 @@
-// 가입안내 페이지
-export default function Page() {
-  return (
-    <main>
-      <h2>Join Page</h2>
-    </main>
-  );
-}

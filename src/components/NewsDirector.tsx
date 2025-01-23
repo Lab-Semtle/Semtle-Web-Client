491d04873285dad3fe35758b5144dbeed7f60f10
한태현 <xogus5343@gmail.com>
2025-01-23 15:21:56 +0900

    UI: NewsDirector 컴포넌트 수정(#4)

--- src/components/NewsDirector.tsx
+++ src/components/NewsDirector.tsx
@@ -12,38 +12,44 @@ type NewsDirectorProps = {
 };
 
 const NewsDirector: React.FC<NewsDirectorProps> = ({
-    imageSrc,
-    altText,
-    newsTitle,
-    newsContent,
-    index = 0, // 기본값 추가
-  }) => {
-    return (
-      <div
-        className={`mb-10 flex ${index % 2 === 1 ? 'flex-row-reverse' : 'flex-row'} items-center gap-6`}
-      >
-        {/* 이미지 카드 */}
-        <Card className="w-[300px]"> {/* 카드 너비 확대 */}
-          <CardHeader className="p-0">
-            <Image
-              src={imageSrc}
-              alt={altText}
-              width={300} 
-              height={180} 
-              className="object-cover w-full h-full" // 너비와 높이를 꽉 채워서 표시
-            />
-          </CardHeader>
-        </Card>
-  
-        {/* 텍스트 영역 */}
-        <div className="flex flex-col gap-2">
-          <Label className={`${index % 2 === 1 ?'ml-[33rem]':''} text-lg font-semibold text-left`}>{newsTitle}</Label>
-          <Label className="text-sm text-muted-foreground text-left">{newsContent}</Label>
-        </div>
+  imageSrc,
+  altText,
+  newsTitle,
+  newsContent,
+  index = 0, // 기본값 추가
+}) => {
+  return (
+    <div
+      className={`mb-10 flex ${index % 2 === 1 ? 'flex-row-reverse' : 'flex-row'} items-center gap-6`}
+    >
+      {/* 이미지 카드 */}
+      <Card className="w-[350px]">
+        {' '}
+        {/* 카드 너비 확대 */}
+        <CardHeader className="p-0">
+          <Image
+            src={imageSrc}
+            alt={altText}
+            width={300}
+            height={180}
+            className="h-full w-full object-cover" // 너비와 높이를 꽉 채워서 표시
+          />
+        </CardHeader>
+      </Card>
+
+      {/* 텍스트 영역 */}
+      <div className="flex flex-col gap-2">
+        <Label
+          className={`${index % 2 === 1 ? 'ml-[31.5rem]' : ''} text-left text-lg font-semibold`}
+        >
+          {newsTitle}
+        </Label>
+        <Label className="text-left text-sm text-muted-foreground">
+          {newsContent}
+        </Label>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
 
 export default NewsDirector;
-

e68c5d6f871f8fe9a43729180936d8f307a27188
or-m-or <htth815@gmail.com>
2025-03-07 20:34:55 +0900

    FIX: Vercel Mixed Content 에러 해결중...메타데이터에 head 추가

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      />
+    </>
+  );
+}

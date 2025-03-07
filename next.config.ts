1bbe0fc59419c6516db5cdd46d8fc73be78aabe6
ormor <htth815@gmail.com>
2025-03-08 00:41:51 +0900

    Fix: Mixed Content 오류 해결 중.. - vercel.json 수정

--- next.config.ts
+++ next.config.ts
@@ -13,14 +13,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // 빌드 시 ESLint 오류 무시
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/v1/:path*', // 외부 API로 프록시할 경로
-        destination: 'http://223.130.140.10:8080/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
